6ba3d2be03d72bf767ed3c33a18547649e52e1bc
TakahiroFujitaSJB <TakahiroTanakaSJB@gmail.com>
2024-06-11 16:01:47 -0600

    changed skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -25,7 +25,7 @@ import csharp from '../assets/img/csharp.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen pb-14 bg-gradient-to-b from-[#1854ad] via-[#4508b4] to-[#b503fc] text-gray-300'>
+    <div name='skills' className='w-full h-screen pt-80 pb-14 bg-gradient-to-b from-[#1854ad] via-[#4508b4] to-[#b503fc] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div className='w-full grid grid-cols-2 sm:grid-cols-2 text-center'>
@@ -34,7 +34,7 @@ const Skills = () => {
             <Lottie animationData={SkillsAni} className='max-h-[400px] max-w-[400px] py-10' />
           </div>
               <p className='py-4 text-lg'>Within my University Career whether it be in a Course or Club, I have learned 
-              and used many technologies. Here are some of them! </p>      
+              and used many techstacks. Here are some of them! </p>      
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center pb-8 pt-4 '>
 
